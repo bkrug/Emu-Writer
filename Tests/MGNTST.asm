5ae59272b27a7222e004b3453c0e4cabc50c5647
Benjamin Krug <benjaminkrug@yahoo.com>
2026-07-02 11:59:57 -0400

    Comment changes

--- Tests/MGNTST.asm
+++ Tests/MGNTST.asm
@@ -1,17 +1,5 @@
-* TODO: The test that I've written will not run yet
-* Part of the problem is the AORG statement in EDTMGN.
-* But even if I remove that, the test is ending up in an infinite loop.
-
-
-* Insert a margin in an empty margin list
-* Insert a margin-entry at the end of a non-empty list
-* Insert a margin-entry at the beginning of a non-empty list
-* Insert a margin-entry in between two entries, where this will not create a duplicate
-* Attempt to insert a margin-entry that would be identical to the one directly after it. The result is to edit the later entry, leaving the list size unchanged.
-* Attempt to insert a margin-entry that would be identical to the one directly before it. The result is to edit the earlier entry, leaving the list size unchanged.
-* Edit an existing margin-entry (same paragraph index).
-* Edit an existing margin-entry such that it will be identical to the following entry. The result will be to delete the following entry, shrinking the list size by one.
-* Edit an existing margin-entry such that it will be identical to the preceding entry. The result will be to delete the later entry, shrinking the list size by one.
+* Tests to add:
+*
 * Undo a margin entry insertion that grew the list size.  (create an entry)
 * Undo a margin entry insertion that really just edit a later entry to point to the current paragraph.  (edit previous entry)
 * Undo a margin entry edit.  (edit current entry)
@@ -63,10 +51,12 @@ TSTLST DATA TSTEND-TSTLST-2/8
 * Edit an existing margin-entry (same paragraph index).
        DATA EDIT1
        TEXT 'EDIT1 '
-* Edit an existing margin-entry such that it will be identical to the following entry. The result will be to delete the following entry, shrinking the list size by one.
+* Edit an existing margin-entry such that it will be identical to the following entry.
+* The result will be to delete the following entry, shrinking the list size by one.
        DATA EDIT2
        TEXT 'EDIT2 '
-* Edit an existing margin-entry such that it will be identical to the preceding entry. The result will be to delete the later entry, shrinking the list size by one.
+* Edit an existing margin-entry such that it will be identical to the preceding entry.
+* The result will be to delete the later entry, shrinking the list size by one.
        DATA EDIT3
        TEXT 'EDIT3 '
 TSTEND
